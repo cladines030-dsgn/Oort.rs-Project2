d018f9e7c05ca1e4ed195bbb1c787b5dc090a905
rrysz <robert-rysz@uiowa.edu>
2026-03-15 11:40:46 -0500

    We <3 Merging!

--- src/main.ts
+++ src/main.ts
@@ -8,11 +8,3 @@ if (!appNode) {
 }
 
 createApplication(appNode);
-
-
-import { createRoot } from "react-dom/client";
-import App from "./app/App.tsx";
-import "./styles/index.css";
-
-createRoot(document.getElementById("root")!).render(<App />);
-  
